f169bb2fc7993cc94cb2b1e382ffeb1e01355241
luminelcy <2323439368@qq.com>
2026-09-07 22:45:18 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.touchingsampleratetest"
-        minSdk = 36
+        minSdk = 35
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
